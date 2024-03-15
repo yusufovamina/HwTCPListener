6253c6c0885dc0c7fd4dbf3865dd2856fde98bf8
Amina <yusufovamina@gmail.com>
2024-03-15 22:49:20 +0400

    Add project files.

---
+++ HwTCPListener/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+
+namespace HwTCPListener
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            var ip = IPAddress.Parse("127.0.0.1");
+            var listener = new TcpListener(ip, 27001);
+            listener.Start(100);
+
+            while (true)
+            {
+                var client = listener.AcceptTcpClient();
+                var stream = client.GetStream();
+                var br = new BinaryReader(stream); //binary reader
+                var bw = new BinaryWriter(stream); //binary writer
+
+                while (true)
+                {
+                    var input = br.ReadString();
+                    var command = JsonSerializer.Deserialize<Command>(input);
+
+                    if (command == null)
+                    {
+                        continue;
+                    }
+                    Console.WriteLine(command.Text);
+                    Console.WriteLine(command.Param);
+                    string[] arr=command.Text.Split(" ");
+
+                    switch (command.Text)
+                    {
+                        case Command.ProcList:
+                            var processes = Process.GetProcesses();
+                            bw.Write(JsonSerializer.Serialize(processes.Select(p => p.ProcessName)));
+                            break;
+                        case Command.Run:
+                            //realization 
+                            break;
+                        case Command.Start:
+                          
+                              
+                            bw.Write(JsonSerializer.Serialize(arr[1] + " is opened"));
+                            break;
+                            
+                        default:
+                            break;
+                    }
+
+                }
+
+            }
+
+
+        }
+
+        public class Command
+        {
+            public const string ProcList = "PROCLIST";
+            public const string Run = "RUN";
+            public const string Start = "START";
+
+            public string Text { get; set; }
+            public string Param { get; set; }
+
+        }
+    }
+}
---
+++ TCPClient/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Diagnostics;
+using System.Net.Sockets;
+using System.Text.Json;
+
+namespace TCPClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new TcpClient();
+            client.Connect("127.0.0.1", 27001);
+
+            var stream = client.GetStream();
+            var br = new BinaryReader(stream);
+            var bw = new BinaryWriter(stream);
+
+            while (true)
+            {
+                var str = Console.ReadLine().ToUpper();
+                if (str == "HELP")
+                {
+                    Console.WriteLine(Command.ProcList);
+                    Console.WriteLine(Command.Run);
+                    Console.WriteLine("HELP");
+                    continue;
+                }
+                Command cmd = null;
+                string response = null;
+
+                var input = str.Split(' ');
+                Console.WriteLine(input[0]);
+                switch (input[0])
+                {
+                    case Command.ProcList:
+                        cmd = new Command { Text = input[0] };
+                        bw.Write(JsonSerializer.Serialize(cmd));
+
+                        response = br.ReadString();
+
+                        var processes = JsonSerializer.Deserialize<string[]>(response);
+
+                        foreach (var process in processes)
+                        {
+                            Console.WriteLine(process);
+                        }
+                        break;
+                    case Command.Run:
+                        //realization 
+                        break;
+                    case Command.Start:
+
+
+                        if (input[1] != null) {
+
+                            cmd = new Command { Text = input[0] };
+                            bw.Write(JsonSerializer.Serialize(cmd));
+                            Process.Start(input[1] + ".exe");
+                        
+                        
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public class Command
+        {
+            public const string ProcList = "PROCLIST";
+            public const string Run = "RUN";
+            public const string Start = "START";
+            public string Text { get; set; }
+            public string Param { get; set; }
+
+        }
+
+    }
+}
